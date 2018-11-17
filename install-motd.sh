486f206c12b6f394f4bf89fc3e67833b1c0c4870
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-11-16 20:12:03 -0900

    Update install-motd.sh
    Silence package installation and display completion banner

--- install-motd.sh
+++ install-motd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get -o Acquire::ForceIPv4=true update -y
-sudo apt-get -o Acquire::ForceIPv4=true install lsb-release update-motd figlet curl -y
+sudo apt-get -o Acquire::ForceIPv4=true update -y -qq
+sudo apt-get -o Acquire::ForceIPv4=true install lsb-release update-motd figlet curl -y -qq
 rm -r /etc/update-motd.d/
 mkdir /etc/update-motd.d/
 touch /etc/update-motd.d/00-header ; touch /etc/update-motd.d/10-sysinfo ; touch /etc/update-motd.d/90-footer
@@ -10,3 +10,4 @@ cat 10-sysinfo > /etc/update-motd.d/10-sysinfo
 cat 90-footer > /etc/update-motd.d/90-footer
 cat issue.net > /etc/issue.net
 systemctl restart sshd
+figlet Installation Complete
